0e69eda660ee5f96b5fcf63230dec0e3db4f1de4
NikBabukhin <nikolai.babukhin@MacBook-Pro-NikolaiBabukhin.local>
2023-08-28 17:46:46 +0300

    Add. Add new icons for battlefield

--- src/Components/MainWrapper/BattlefieldWrapper/Battlefield/Battlefield.tsx
+++ src/Components/MainWrapper/BattlefieldWrapper/Battlefield/Battlefield.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import style from "./Battlefield.module.css"
+import style from "./Battlefield.module.css";
+import crossImage from "./../../../../icons/xmark-solid.svg";
+import fireImage from "./../../../../icons/fire-fill.svg";
+import explosionImage from "./../../../../icons/explosion.svg";
 
 type BattlefieldPropsType = {
     side: 'user' | 'enemy'
 }
 
+
+const state = {
+    enemy: ['baseEnemy', 'missShotEnemy', 'hitShotEnemy', 'deadShipEnemy'],
+    ally: ['baseAllied', 'missShotAllied', 'hitShotAllied', 'deadShipAllied', 'livingShipAllied']
+}
+
+
 export const Battlefield: React.FC<BattlefieldPropsType> = (props) => {
+
+
     return <div className={`${style.battlefield} ${props.side}`}>
         <table border={1}>
             <tr>
@@ -21,18 +33,24 @@ export const Battlefield: React.FC<BattlefieldPropsType> = (props) => {
             </tr>
             <tr>
                 <td>A</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td className={style.baseEnemy}></td>
+                <td className={style.missShotEnemy}>
+                    <img src={crossImage} width={"100%"} height={"100%"}/>
+                </td>
+                <td className={style.hitShotEnemy}>
+                    <img src={fireImage} width={"100%"} height={"100%"}/>
+                </td>
+                <td className={style.deadShipEnemy}>
+                    <img src={explosionImage} width={'100%'} height={'100%'}/>
+                </td>
+                <td className={style.missShotAllied}></td>
+                <td className={style.hitShotAllied}></td>
+                <td className={style.deadShipAllied}></td>
+                <td className={style.livingShipAllied}></td>
             </tr>
             <tr>
                 <td>B</td>
-                <td></td>
+                <td className={style.baseAllied}></td>
                 <td></td>
                 <td></td>
                 <td></td>
--- src/Components/MainWrapper/MainWrapper.tsx
+++ src/Components/MainWrapper/MainWrapper.tsx
@@ -1,4 +1,3 @@
-import React, {useState} from "react";
 import style from './MainWrapper.module.css';
 import {BattlefieldWrapper} from "./BattlefieldWrapper/BattlefieldWrapper";
 import {InterfaceArea} from "./InterfaceArea/InterfaceArea";
@@ -6,20 +5,12 @@ import {ConsoleArea} from "./ConsoleArea/ConsoleArea";
 
 
 export const MainWrapper:React.FC<any> = () => {
-    const [isVisible, setVisible] = useState(false)
 
-    const onClickHandler = () => {
-        setVisible(!isVisible)
-    }
-
-    return <>
-        <button onClick={onClickHandler}>Click</button>
-        <div className={style.wrapper}>
+    return <div className={style.wrapper}>
             <div className={style.main_content}>
                 <BattlefieldWrapper/>
                 <InterfaceArea/>
             </div>
-            <ConsoleArea isVisible={isVisible}/>
+            <ConsoleArea isVisible={true}/>
         </div>
-    </>
 }
